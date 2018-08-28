f01fc9ddf4edad753d16a31f592c4674f7bf9f5a
XIAO小恋歌 <353673246@qq.com>
2018-08-28 16:28:50 +0800

    webpack simple demo

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app">
+    <p>
+      {{ value }}
+      <myComponent></myComponent>  
+    </p>
+  </div>
+</template>
+
+<script>
+import myComponent from './components/MyComponent.vue'
+import "./style/app.scss"
+export default {
+  data(){
+    return {
+      value:12345
+    }
+  },
+  components:{
+      myComponent
+  }
+}
+</script>
+
+<style lang="scss">
+.app{
+    p{
+      font-size: 100px;
+    color:red
+    }
+    
+  
+  
+}
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        child
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.app{
+    font-size: 200px;
+}
+</style>
+
